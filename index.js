90b7b3a31fd75836447a27a4aa82e0cef4b4d5cd
sami5671 <samialam5671@gmail.com>
2023-12-22 15:53:30 +0600

    added the email query api

--- index.js
+++ index.js
@@ -24,7 +24,40 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    // await client.connect();
+    await client.connect();
+
+    // =======================All collections are here ==========================================
+
+    const AllUserTasksCollection = client
+      .db("ToDoList")
+      .collection("AllUserTasks");
+
+    // =================================================================
+    // console.log(email);
+    // let query = {};
+    // if (req.query.email) {
+    //   query = { ProductOwnerEmail: email };
+    // }
+    // console.log(query);
+    // ======================TO DO LIST ===========================================
+    app.post("/AllUserTasks", async (req, res) => {
+      const item = req.body;
+      const result = await AllUserTasksCollection.insertOne(item);
+      res.send(result);
+    });
+    app.get("/AllUserTasks", async (req, res) => {
+      const result = await AllUserTasksCollection.find().toArray();
+      res.send(result);
+    });
+
+    app.get("/userAddedTaskByEmail", async (req, res) => {
+      const email = req.query.email;
+      const query = { TaskUserEmail: email };
+      const result = await AllUserTasksCollection.find(query).toArray();
+      console.log(result);
+      res.send(result);
+    });
+    // =================================================================
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
@@ -32,7 +65,7 @@ async function run() {
     );
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
